081b3625cbe101129a98f394887f325a66433d29
zkan <zkan.cs@gmail.com>
2021-12-11 21:16:48 +0700

    Create new todo item

--- app/controllers/TodoListController.scala
+++ app/controllers/TodoListController.scala
@@ -15,6 +15,7 @@ class TodoListController @Inject()(val controllerComponents: ControllerComponent
     todoList += TodoListItem(2, "Call mom", false)
 
     implicit val todoItemJsonFormat = Json.format[TodoListItem]
+    implicit val newTodoItemJsonFormat = Json.format[NewTodoItem]
 
     def all(): Action[AnyContent] = Action {
         if (todoList.isEmpty) {
@@ -31,4 +32,20 @@ class TodoListController @Inject()(val controllerComponents: ControllerComponent
             case None => NotFound
         }
     }
+
+    def newItem() = Action { implicit request =>
+        val jsonObject = request.body.asJson
+        val newTodoItem: Option[NewTodoItem] = jsonObject.flatMap(Json.fromJson[NewTodoItem](_).asOpt)
+
+        newTodoItem match {
+            case Some(newItem) =>
+                val nextId = todoList.map(_.id).max + 1
+                val toBeAdded = TodoListItem(nextId, newItem.description, false)
+                todoList += toBeAdded
+                Created(Json.toJson(toBeAdded))
+            case None =>
+                BadRequest
+        }
+    }
+
 }
